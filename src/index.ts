145f53ec4836d87e6b813e4d782adfb02374d3f3
Ru Chern Chong <hello@ruchern.dev>
2024-11-12 00:15:45 +0800

    Add Hono middleware

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { routes } from "./api/routes";
 import { errorHandler } from "./api/middleware/errorHandler";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
+import { timing } from "hono/timing";
 
 const app = new Hono();
 
 app.use(logger());
 app.use(prettyJSON());
+app.use(timing());
 
 app.onError(errorHandler);
 
