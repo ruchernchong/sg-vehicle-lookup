c0cdd2583fb6b034235dfda96137334f1353ad9c
Ru Chern Chong <hello@ruchern.dev>
2024-11-15 21:58:50 +0800

    Update special plates

--- src/config/plates.config.ts
+++ src/config/plates.config.ts
@@ -43,37 +43,176 @@ export const SPECIAL_PLATES: SpecialPlatesMap = {
   "S/CD": {
     plate: "S/CD",
     description: "Diplomatic Corps vehicles",
-    category: "diplomaticCorps",
+    category: "Diplomatic Corps",
     categoryDescription: "Foreign mission vehicles",
   },
   "S/CC": {
     plate: "S/CC",
     description: "Consular Corps vehicles",
-    category: "diplomaticCorps",
+    category: "Diplomatic Corps",
     categoryDescription: "Foreign mission vehicles",
   },
   "S/TE": {
     plate: "S/TE",
     description: "Technical employment vehicles",
-    category: "diplomaticCorps",
+    category: "Diplomatic Corps",
     categoryDescription: "Foreign mission vehicles",
   },
   LTA: {
     plate: "LTA",
     description: "Land Transport Authority enforcement vehicles",
-    category: "governmentAgencies",
+    category: "Government Agencies",
     categoryDescription: "Official government agency vehicles",
   },
   MID: {
     plate: "MID",
     description: "Singapore Armed Forces vehicles",
-    category: "governmentAgencies",
+    category: "Government Agencies",
     categoryDescription: "Official government agency vehicles",
   },
   MP: {
     plate: "MP",
     description: "Military Police vehicles",
-    category: "governmentAgencies",
+    category: "Government Agencies",
     categoryDescription: "Official government agency vehicles",
+  },
+  ASN: {
+    plate: "ASN",
+    description:
+      "Vehicles used by VIPs and delegates during the ASEAN Summit held in Singapore in November 2018",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  WTO: {
+    plate: "WTO",
+    description:
+      "Vehicles used during the World Trade Organization's inaugural Ministerial Conference held in Singapore in December 1996",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  IOC: {
+    plate: "IOC",
+    description:
+      "Vehicles used during the International Olympic Committee's 117th Session held in Singapore in July 2005",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  NDP: {
+    plate: "NDP",
+    description:
+      "Vehicles used during the National Day Parade, 2005, on 9 August 2005",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  AIRSHOW: {
+    plate: "AIRSHOW",
+    description: "Vehicles used during Singapore Airshow",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  APEC: {
+    plate: "APEC",
+    description:
+      "Vehicles used during the APEC Annual Meetings in November 2009",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  SIWW: {
+    plate: "SIWW",
+    description: "Vehicles used during Singapore International Water Week",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  WCS: {
+    plate: "WCS",
+    description: "Vehicles used during World Cities Summit",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  YOG: {
+    plate: "YOG",
+    description: "Vehicles used during the 2010 Summer Youth Olympics",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  SEAG: {
+    plate: "SEAG",
+    description: "Vehicles used during the 2015 Southeast Asian Games",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  APG: {
+    plate: "APG",
+    description: "Vehicles used during the 2015 ASEAN Para Games",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  HSBC: {
+    plate: "HSBC",
+    description:
+      "Vehicles used during the events organised by HSBC (Hongkong and Shanghai Banking Corporation)",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  HWWC: {
+    plate: "HWWC",
+    description: "Vehicles used during the HSBC Women's World Championship",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  OCBC: {
+    plate: "OCBC",
+    description: "Vehicles used by OCBC during annual cycling event",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  SGP: {
+    plate: "SGP",
+    description: "Vehicles used during the Singapore Grand Prix",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  IMDEX: {
+    plate: "IMDEX",
+    description:
+      "Vehicles used during the 2017 Maritime Defence Exhibition & Conference (IMDEX Asia 2017)",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  CESS: {
+    plate: "CESS",
+    description: "Vehicles used during the CleanEnviro Summit",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  IW: {
+    plate: "IW",
+    description: "Vehicles used during the Interpol World",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  LIV: {
+    plate: "LIV",
+    description: "Vehicles used during the LIV Golf Tournament",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  GYSS: {
+    plate: "GYSS",
+    description: "Vehicles used during the Global Young Scientists Summit",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  MEDIATE: {
+    plate: "MEDIATE",
+    description: "Vehicles used during the Singapore Convention on Mediation",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
+  },
+  SIN: {
+    plate: "SIN",
+    description: "Vehicles used during the 2023 Singapore Classic",
+    category: "Events",
+    categoryDescription: "Vehicles for special events and conferences",
   },
 };
